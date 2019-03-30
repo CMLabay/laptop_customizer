e48a03115c00c7746e804a57311d668021906a03
Christina Labay <havel.christina@gmail.com>
2019-03-30 08:10:37 -0500

    Change to function component

--- src/Summary/Summary.js
+++ src/Summary/Summary.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-class Summary extends Component{
-    render(){
-        return Object.keys(this.props.selected)
-                .map(key => 
-                <div className="summary__option" key={key}>
-                    <div className="summary__option__label">{key}  </div>
-                    <div className="summary__option__value">{this.props.selected[key].name}</div>
-                    <div className="summary__option__cost">
-                        { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                        .format(this.props.selected[key].cost) }
-                    </div>
-                </div>)
-    }
+function Summary(props){
+    return Object.keys(props.selected)
+            .map(key => 
+            <div className="summary__option" key={key}>
+                <div className="summary__option__label">{key}  </div>
+                <div className="summary__option__value">{props.selected[key].name}</div>
+                <div className="summary__option__cost">
+                    { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                    .format(props.selected[key].cost) }
+                </div>
+            </div>)
 }
 export default Summary;
