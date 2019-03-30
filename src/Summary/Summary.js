49aa5a44660375e0b60fc4769864c632acc5434b
Christina Labay <havel.christina@gmail.com>
2019-03-30 08:35:34 -0500

    Split into Summary and SummaryList

--- src/Summary/Summary.js
+++ src/Summary/Summary.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import SummaryList from '../SummaryList/SummaryList';
 
 function Summary(props){
     return Object.keys(props.selected)
             .map(key => 
-            <div className="summary__option" key={key}>
-                <div className="summary__option__label">{key}  </div>
-                <div className="summary__option__value">{props.selected[key].name}</div>
-                <div className="summary__option__cost">
-                    { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                    .format(props.selected[key].cost) }
-                </div>
-            </div>)
+                <SummaryList 
+                    key={key}
+                    name={props.selected[key].name}
+                    cost={props.selected[key].cost}
+                />)
+            
 }
 export default Summary;
